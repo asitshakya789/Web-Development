aec56149e7395b5447fc923886983111c5d25ff6
asitshakya678 <asitshakya789@gmail.com>
2024-10-02 23:02:09 +0530

    day 1

---
+++ Backend NodeJS/Day 1/comments.js
@@ -0,0 +1,6 @@
+// this is some randome comments    simgle line
+// this is some randome comments
+// this is some randome comments
+/*j rgjtr ngijtr erkn
+k e kle ff df,c k customElementsk fjr ErrorEventm fjr                multipal line 
+j fj rjg removeEventListener,m erj re frameElement.ATTRIBUTE_NODE*/
