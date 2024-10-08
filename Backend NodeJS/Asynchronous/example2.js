917b319e049c2b54635a302eb24f9341208ad97a
asitshakya678 <asitshakya789@gmail.com>
2024-10-08 21:11:09 +0530

    Asynchronous JavaScript

---
+++ Backend NodeJS/Asynchronous/example2.js
@@ -0,0 +1,11 @@
+console.log("Starting");
+for(let i =0 ; i<= 3;i++){
+    setTimeout(() => {
+        console.log('timer done')
+    }, 10);
+}
+
+for(let i =0 ;i <=10000000000 ; i++){
+
+}
+console.log("End");
