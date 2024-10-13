73009c13e3f626abbaa5b126d7d79f6c8f63710b
asitshakya678 <asitshakya789@gmail.com>
2024-10-13 10:16:47 +0530

    added to curd operation create file

---
+++ Backend NodeJS/create.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const dirpath = path.join(__dirname, "Curd");
+const filepath = `${dirpath}/text.txt`;
+
+// fs.writeFile(filepath, "hey my name is asit kumar", "utf8", (err) => {
+//   // If you want to read the file after writing, you can do so here
+//   fs.readFile(filepath, "utf8", (err, data) => {
+//     if (!err) console.log("File written successfully:");
+//   });
+// });
+fs.appendFile(filepath,'this is text to added into tge file', (err, data) => {
+  if (err) throw err;
+  console.log("Data appended successfully!");
+})
